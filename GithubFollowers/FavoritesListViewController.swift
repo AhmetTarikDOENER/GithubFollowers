3558cc32d30bf38ac1e225e4ddd4d4c7f8ff9223
Ahmet Tarik DÖNER <tarik.iosdev@icloud.com>
2023-10-11 20:00:50 +0300

    Added basic navigation controllers.

---
+++ GithubFollowers/FavoritesListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FavoritesListViewController.swift
+//  GithubFollowers
+//
+//  Created by Ahmet Tarik DÖNER on 11.10.2023.
+//
+
+import UIKit
+
+class FavoritesListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemMint
+        title = "Favorites"
+    }
+}
---
+++ GithubFollowers/SearchViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SearchViewController.swift
+//  GithubFollowers
+//
+//  Created by Ahmet Tarik DÖNER on 11.10.2023.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+        title = "Search"
+    }
+}
