6c3e71931cb22f39418ccdf8c7c6303a558da7a1
Ahmet Tarik DÖNER <tarik.iosdev@icloud.com>
2023-10-11 16:52:48 +0300

    GithubFollowers project created.

---
+++ GithubFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubFollowers
+//
+//  Created by Ahmet Tarik DÖNER on 10.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
