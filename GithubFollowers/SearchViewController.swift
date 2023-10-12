4b95511192b15e4be8202f121fd44791e90e9049
Ahmet Tarik DÖNER <tarik.iosdev@icloud.com>
2023-10-12 19:16:07 +0300

    Added custom components to the search screen.

--- GithubFollowers/SearchViewController.swift
+++ GithubFollowers/SearchViewController.swift
@@ -9,9 +9,56 @@ import UIKit
 
 class SearchViewController: UIViewController {
     
+    private let githubImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(named: "gh-logo")
+        
+        return imageView
+    }()
+    
+    private let usernameTextField: GFCustomTextField = {
+        let textField = GFCustomTextField()
+        return textField
+    }()
+    
+    private let customActionButton = GFCustomButton(backgroundColor: .systemGreen, title: "Get Followers")
+    
+    
+    //MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBlue
+        view.backgroundColor = .systemBackground
+        view.addSubViews(githubImageView, usernameTextField, customActionButton)
+        addConstraints()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    
+    //MARK: - Private
+    
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            githubImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80),
+            githubImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            githubImageView.heightAnchor.constraint(equalToConstant: 200),
+            githubImageView.widthAnchor.constraint(equalToConstant: 200),
+            
+            usernameTextField.topAnchor.constraint(equalTo: githubImageView.bottomAnchor, constant: 48),
+            usernameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            usernameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40),
+            usernameTextField.heightAnchor.constraint(equalToConstant: 50),
+            
+            customActionButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50),
+            customActionButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            customActionButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40),
+            customActionButton.heightAnchor.constraint(equalToConstant: 50),
+        ])
     }
     
 }
